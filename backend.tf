92ca0e7393c07acc779db64ac1aa3ff0b614646e
vinithmh <vinithmh2000@gmail.com>
2024-06-03 19:29:54 +0530

    scsd

---
+++ backend.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket         = "s3-revhire-backend-job-remote-new-cluster"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "revhire-job-backend-dynamodb-new-cluster"
+  }
+}
+
+# qwbwjh
+
